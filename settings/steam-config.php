264892cb044d5402bf77579f1c43901e2444cc17
Xing Long <angxinglong@gmail.com>
2019-04-16 21:14:37 +0800

    Update steam-config.php

--- settings/steam-config.php
+++ settings/steam-config.php
@@ -1,4 +1,4 @@
 			<?php
 			
-            $steamauth['apikey'] = "9A269EA764F207BA8B590C8B34BFDDF3"; 
-			$steamauth['domainname'] = "LocalHost"; 
+            $steamauth['apikey'] = ""; 
+		$steamauth['domainname'] = "LocalHost"; 
